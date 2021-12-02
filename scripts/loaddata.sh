4fbcc14216c53c4772f7a3505f5c059987e1c31f
olivierdalang <olivier.dalang@gmail.com>
2021-12-02 16:05:15 +0100

    users fixtures

--- scripts/loaddata.sh
+++ scripts/loaddata.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+# create demo data
 docker-compose exec django python manage.py loaddata sector5 sector4 sector3 sector2 plots section treatment inventory
 docker-compose exec django python manage.py rebuild_sectors_tree
+
+# create default groups and users (admin//test, manager//test, worker//test)
+docker-compose exec django python manage.py loaddata demo_users
