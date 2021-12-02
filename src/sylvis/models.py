7fbad9da2e9a0d75778e74d0733399d9ab36e875
olivierdalang <olivier.dalang@gmail.com>
2021-12-02 10:03:09 +0100

    model

--- src/sylvis/models.py
+++ src/sylvis/models.py
@@ -11,6 +11,8 @@ class Sector(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default="")
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
     @property
     def section_set(self):
@@ -36,13 +38,25 @@ class Plot(models.Model):
         verbose_name = " " + _("Plot")
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    geom = models.PolygonField(srid=2056)
-    name = models.CharField(max_length=255)
+    geom = models.PolygonField(srid=2056, verbose_name=_("Geometry"))
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
     sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, blank=True, null=True)
-    rotation_sections = models.IntegerField(default=0)
-    rotation_treatments = models.IntegerField(default=0)
-    planned_next_section = models.DateField(blank=True, null=True)
-    planned_next_treatment = models.DateField(blank=True, null=True)
+    rotation_sections = models.IntegerField(
+        default=0, verbose_name=_("Section rotation rate")
+    )
+    rotation_treatments = models.IntegerField(
+        default=0, verbose_name=_("Treatment rotation rate")
+    )
+    planned_next_section = models.DateField(
+        blank=True, null=True, verbose_name=_("Next planned section")
+    )
+    planned_next_treatment = models.DateField(
+        blank=True, null=True, verbose_name=_("Next planned treatment")
+    )
+    yearly_growth = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name=_("Yearly growth"), default=0
+    )
+    etale = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse("admin:plot_view", args=[self.id])
