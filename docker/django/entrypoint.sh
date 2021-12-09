6213fcc9d6b509bb542e719e6b85038419d44315
olivierdalang <olivier.dalang@gmail.com>
2021-12-09 10:27:45 +0100

    don't fail on transifex failure

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -4,8 +4,13 @@ set -e
 
 # TODO: unsure it's good practice to do that here since
 # failing transifex would prevent starting the container.
-tx pull -a
-python manage.py compilemessages
+if tx pull -a; then
+    echo "Command succeeded"
+    python manage.py compilemessages
+else
+    echo "Failed to pull Transifex translations !"
+    echo "Translations will not be available. Make sure you properly set the TX_TOKEN variable."
+fi
 
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
